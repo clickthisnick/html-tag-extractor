41963bbec88225045717e28b71e58ca46e1fa65f
Nick Smith <smith.nicholas.isu@gmail.com>
2017-02-03 10:24:29 -0500

    Adding YAML config file

--- README.md
+++ README.md
@@ -2,10 +2,26 @@
 This extracts unique tags from any html files inside a directory
 
 # Usage:
-node code.js --path=[pathToFolder] --tag=[theTagToExtract]
+First run npm install to download get the pacakges from npm
+Second go into config.yml and configure for your use case
 
-# Example:
-node code.js --path=/Users/Desktop/website --tag=button
+The options are:
+path: This is the directory we will walk
+ignoreDirectories: Directories with this name will be ignored
+tag: This is the tag <*></*> That we are searching for
+delimiter: This string will be in between each html element
+fileExtensionsToScan: This is an array of file extensions we want to scan ['.html', '.js']
+
+# Example
+config.yml:
+path: "."
+ignoreDirectories: ["node_modules"]
+tag: "button"
+delimiter: "<br>"
+fileExtensionsToScan: [".html"]
+
+Run in terminal:
+node code.js
 
 This code would walk through the /Users/Desktop/website directory and extract any <button></button> button elements from html files
 
--- code.js
+++ code.js
@@ -1,76 +1,34 @@
-const argv = require('yargs').argv,
-    walk = require('walk'),
+const walk = require('walk'),
     fs = require('fs'),
     _ = require('lodash'),
+    YAML = require('yamljs'),
+    cfg = YAML.load('config.yml');
     htmlparser = require('htmlparser');
-
-// Ask the user to specify the path to start walking the code
-if (_.isNil(argv.path)) {
-    throw new Error('Could not find --path=');
-}
-
-// Ask the user to specify an elment tag, like button for <button></button>
-if (_.isNil(argv.tag)) {
-    throw new Error('Could not find --tag=');
-}
-
-// Add an options args for delimiter (join by) and if you want text between the <></> tags
 
 // The list of elements
 let elements = [];
 
 function walkThroughChildren(element) {
-    if (element.name === argv.tag) {
-        elements.push(element);
+    // Push unique element html to our elements array
+    if (element.name === cfg.tag) {
+        const rawElementHtml = `<${element.raw}></${element.name}>`
+        elements.push(rawElementHtml);
     }
 
     // Keep walking through the elements children
-    if (!_.isNil(element.children)) {
-        _.forEach(element.children, (child) => {
-            walkThroughChildren(child);
-        });
-    }
-}
-
-function generateElements() {
-    let uniqueElementsHtmlArray = [];
-
-    _.forEach(elements, (element) => {
-        let elementHtml = `<${element.name}`;
-
-        // Add a space if element has attributes
-        if (!_.isNil(element.attribs)) {
-            elementHtml += ' ';
-        }
-
-        _.forEach(_.keys(element.attribs), (attribute, idx) => {
-            elementHtml += `${attribute}='${element.attribs[attribute]}'`;
-
-            // If the attribute is the last attribute do not add an extra space
-            if (!idx === _.keys(element.attribs).length) {
-                elementHtml += ' ';
-            }
-        });
-        elementHtml += `></${element.name}>`;
-        if (!_.includes(uniqueElementsHtmlArray, elementHtml)) {
-            uniqueElementsHtmlArray.push(elementHtml);
-        }
-    });
-    return _.join(uniqueElementsHtmlArray, '');
+    _.forEach(element.children, (child) => walkThroughChildren(child));
 }
 
 let options = {
+    // These directories will be ignored
+    filters: cfg.ignoreDirectories,
     listeners: {
         file: function(root, fileStats, next) {
-            // Just printing this out so I can see the script is still working
-            console.log(fileStats.name);
-            if (_.endsWith(fileStats.name, '.html')) {
+            if (_.includes(fileStats.name, cfg.fileExtensionsToScan)) {
                 fs.readFile(`${root}/${fileStats.name}`, (err, data) => {
                     const rawHtml = data.toString('utf8'),
-                        handler = new htmlparser.DefaultHandler(function(error, dom) {
-                            if (!error) {
-                                _.forEach(dom, (element) => walkThroughChildren(element));
-                            }
+                        handler = new htmlparser.DefaultHandler((error, dom) => {
+                            _.forEach(dom, (element) => walkThroughChildren(element));
                         }),
                         parser = new htmlparser.Parser(handler);
 
@@ -80,7 +38,7 @@ let options = {
             next();
         },
         end: function() {
-            console.log(generateElements());
+            console.log(_.join(_.uniq(elements), cfg.delimiter));
         },
         errors: function(root, nodeStatsArray, next) {
             next();
@@ -89,4 +47,4 @@ let options = {
 };
 
 // Instantiate the walker to walk the directory
-walk.walk(argv.path, options);
+walk.walk(cfg.path, options);
